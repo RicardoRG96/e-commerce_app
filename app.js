641cc85b0246f30b3c607b778643acb9b36a5d75
RicardoRG96 <ricardoretamal10@gmail.com>
2024-06-07 19:21:07 -0400

    añade registro e inicio de sesion de usuarios

--- app.js
+++ app.js
@@ -5,13 +5,10 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var accountRouter = require('./routes/account');
+var productsRouter = require('./routes/products');
 
 var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -20,7 +17,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/api/account', accountRouter);
+app.use('/api/products', productsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -35,7 +33,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.send('error: ' + err);
 });
 
 module.exports = app;
--- db/services.js
+++ db/services.js
@@ -1,1 +1,46 @@
 const db = require('./config');
+
+function requestOne(table, id, callback) {
+    const sql = `SELECT * FROM ${table} WHERE id = ${id}`;
+
+    db.any(sql)
+        .then(([result]) => {
+            callback(null, result);
+        })
+        .catch(err => {
+            callback(err);
+        })
+}
+
+function getUserCredentials(table, email, callback) {
+    const sql = `SELECT email, password FROM ${table} WHERE email = '${email}'`;
+
+    db.any(sql)
+        .then(([result]) => {
+            callback(null, result);
+        })
+        .catch(err => {
+            callback(err);
+        })
+}
+
+function insertItem(table, item, callback) {
+    const keys = Object.keys(item);
+    const properties = keys.join(', ');
+    const values = keys.map(key => `'${item[key]}'`).join(', ');
+    const sql = `INSERT INTO ${table} (${properties}) VALUES(${values}) RETURNING *`;
+
+    db.any(sql)
+        .then(([result]) => {
+            callback(null, result);
+        })
+        .catch(err => {
+            callback(err);
+        })
+}
+
+module.exports = {
+    requestOne,
+    insertItem,
+    getUserCredentials
+}
