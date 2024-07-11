c4941e5cffff5d62b19f6d2a860af65f5f6cbfb9
RicardoRG96 <ricardoretamal10@gmail.com>
2024-07-11 17:12:27 -0400

    añade ruta endpoint para cambiar contraseña

--- routes/myAccount.js
+++ routes/myAccount.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var router = express.Router();
-const { requestOne, getOrderDetails } = require('../db/services');
-const { query, validationResult } = require('express-validator');
+const bcrypt = require('bcrypt');
+const { requestOne, getOrderDetails, updateItem } = require('../db/services');
+const { query, validationResult, body } = require('express-validator');
 const verifyToken = require('../utils');
 
 /* GET users listing. */
@@ -50,5 +51,93 @@ router.get('/:id', verifyToken, function(req, res, next) {
     res.status(200).json(user);
   });
 });
+
+router.put('/update-user-info',
+  body('userId').isInt().escape(),
+  body('name').notEmpty().escape(),
+  body('email').isEmail().notEmpty(),
+  verifyToken,
+  function(req, res, next) {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array()})
+    }
+
+    const { userId } = req.body;
+    const nextUserCredentials = {
+      name: req.body.name,
+      email: req.body.email
+    }
+    requestOne('users', 'id', userId, (err, userData) => {
+      if (err) {
+        return next(err);
+      }
+      if (!userData.length) {
+        return res.status(404);
+      }
+      updateItem('users', userId, nextUserCredentials, (err, updatedData) => {
+        if (err) {
+          return next(err);
+        }
+        res.status(200).json(updatedData);
+      });
+    })
+  }
+)
+
+router.put('/update-password',
+  body('userId').isInt().escape(),
+  body('currentPassword').notEmpty().escape(),
+  body('newPassword').notEmpty().escape(),
+  verifyToken,
+  async function(req, res, next) {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      const { userId, currentPassword, newPassword } = req.body;
+    
+      requestOne('users', 'id', userId, (err, userCredentials) => {
+        if (err) {
+          return next(err);
+        }
+        if (!userCredentials.length) {
+          return res.status(404);
+        }
+        bcrypt.compare(currentPassword, userCredentials[0].password)
+          .then(matchPassword => {
+            if (matchPassword){
+              const saltRounds = 10;
+              bcrypt.genSalt(saltRounds)
+                .then(salt => {
+                  return bcrypt.hash(newPassword, salt)
+                })
+                .then(hashedPassword => {
+                  const nextUserPassword = {
+                    password: hashedPassword
+                  }
+                  updateItem('users', userId, nextUserPassword, (err, updatedPassword) => {
+                    if (err) {
+                      return next(err);
+                    }
+                    return res.status(200).json({ message: 'password changed successfuly' });
+                  })
+                })
+                .catch(err => {
+                  return res.status(404).json({ error: err })
+                })
+            } else return res.sendStatus(404)
+          })
+          .catch(err => {
+            return res.status(404).json({ error: err })
+          })
+      })  
+    } catch (err) {
+      return res.status(500).json({ message: 'Internal server error', error: err });
+    }
+  }
+)
 
 module.exports = router;
